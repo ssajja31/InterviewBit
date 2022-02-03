f7fed0a996d2ca7717fad0f9fb6040f3c8f8b802
ssajja31 <91031396+ssajja31@users.noreply.github.com>
2022-02-03 11:43:07 -0700

    Kth Row of Pascal Triangle

---
+++ Arrays/KThRowOfPascalTriangle.cpp
@@ -0,0 +1,15 @@
+vector<int> Solution::getRow(int A) {
+    vector<int> res(A+1, 0);
+
+    res[0] = 1; 
+ 
+    for (int i = 1; i <= A; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            res[j] = res[j] + res[j - 1];
+        }
+    }
+    
+    return res;
+}
