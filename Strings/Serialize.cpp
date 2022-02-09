0551530a92e19de8c69e2e201829fe27f0f86bde
ssajja31 <91031396+ssajja31@users.noreply.github.com>
2022-02-09 10:51:25 -0700

    Serialize a string

---
+++ Strings/Serialize.cpp
@@ -0,0 +1,8 @@
+string Solution::serialize(vector<string> &A) {
+    string res = "";
+    for(int i = 0; i < A.size(); i++)
+    {
+        res = res + A[i] + to_string(A[i].size()) + "~";
+    }
+    return res;
+}
